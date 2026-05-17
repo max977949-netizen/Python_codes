7842f61cf9b7b9fd12daefc8926979277f5a64e0
max977949-netizen <max977949@gmail.com>
2026-05-17 14:27:32 -0600

    Add files via upload
    un medidor de temperatura que usa valores random

---
+++ main (1).cpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <thread>
+using std::cout;
+using std::cin;
+using std::endl;
+
+void revisarTemperatura(int temperatura){
+    cout<<"revisando temperatura. Temperatura actual "<<temperatura<<"Cº"<<endl;
+    if(temperatura >= 30){
+        cout<<"Alerta temperatura alta"<<endl;
+    }
+    else{
+        cout<<"temperatura normal"<<endl;
+    }
+}
+
+
+int main()
+{
+   srand(time(0));
+   while(true){
+        int tempSimulada1 = (rand()%26) + 15;
+        int tempSimulada2 = (rand()%26) + 15;
+        revisarTemperatura(tempSimulada1);
+        revisarTemperatura(tempSimulada2);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+   }
+
+    return 0;
+}
